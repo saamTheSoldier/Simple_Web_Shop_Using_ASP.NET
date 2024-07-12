1794541e573c7f25b032e5afc8862f2955335fab
mhh.hosseini <soldier.mh.2001@gmail.com>
2024-07-12 17:25:18 +0330

    program.cs changes to have the proper route

--- Program.cs
+++ Program.cs
@@ -23,5 +23,9 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "AdminPanel/{action=AdminPanel}/{id?}",
+    defaults: new { Controller = "AdminPanel" });
 
 app.Run();
